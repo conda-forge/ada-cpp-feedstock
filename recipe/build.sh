1acb4dcf9b8184a49b19b6ec3e63101e7e7bfe70
krande <kristoffer_andersen@outlook.com>
2026-06-02 12:41:24 +0200

    Remove unused patch file; update CMake-based build and recipe dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 set -ex
-# if darwin print sdk version
-if [[ $(uname) == Darwin ]]; then
-  echo $CMAKE_ARGS
-  xcrun --show-sdk-version
-fi
 
-python -m pip install . --no-build-isolation -v \
-    --config-settings=cmake.args=-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    --config-settings=cmake.args=-DBUILD_PYTHON=ON \
-    --config-settings=cmake.args=-DBUILD_STP2GLB=ON \
-    --config-settings=cmake.args=-DSTP2GLB_BIN_DIR="${PREFIX}/bin" \
-    --config-settings=cmake.args=-DBUILD_WASM=OFF
+# Build with CMake
+cmake -B build -S . -G Ninja ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_PYTHON=ON \
+    -DBUILD_STP2GLB=ON \
+    -DSTP2GLB_BIN_DIR="${PREFIX}/bin" \
+    -DBUILD_WASM=OFF \
+    -DPYTHON_EXECUTABLE="${PYTHON}" \
+    -DPYTHON_INCLUDE="${PREFIX}/include" \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+    -DPYTHON_SITE_PACKAGES="${SP_DIR}"
+
+cmake --build build
+cmake --install build
+
+# Install Python package files
+${PYTHON} -m pip install . --no-deps --no-build-isolation -vv
